08ddae9a461f6c537730ff78f7561a7119bd9c7e
Daniel Micay <danielmicay@gmail.com>
2016-07-16 23:55:16 -0400

    replace VLA formatting with dprintf-like function
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- libc/bionic/bionic_systrace.cpp
+++ libc/bionic/bionic_systrace.cpp
@@ -26,63 +26,15 @@
 
 #include <cutils/trace.h> // For ATRACE_TAG_BIONIC.
 
-#define WRITE_OFFSET   32
-
-static Lock g_lock;
 static CachedProperty g_debug_atrace_tags_enableflags("debug.atrace.tags.enableflags");
-static uint64_t g_tags;
-static int g_trace_marker_fd = -1;
-
-static bool should_trace() {
-  g_lock.lock();
-  if (g_debug_atrace_tags_enableflags.DidChange()) {
-    g_tags = strtoull(g_debug_atrace_tags_enableflags.Get(), nullptr, 0);
-  }
-  g_lock.unlock();
-  return ((g_tags & ATRACE_TAG_BIONIC) != 0);
-}
-
-static int get_trace_marker_fd() {
-  g_lock.lock();
-  if (g_trace_marker_fd == -1) {
-    g_trace_marker_fd = open("/sys/kernel/debug/tracing/trace_marker", O_CLOEXEC | O_WRONLY);
-  }
-  g_lock.unlock();
-  return g_trace_marker_fd;
-}
 
 void bionic_trace_begin(const char* message) {
-  if (!should_trace()) {
     return;
-  }
-
-  int trace_marker_fd = get_trace_marker_fd();
-  if (trace_marker_fd == -1) {
-    return;
-  }
-
-  // If bionic tracing has been enabled, then write the message to the
-  // kernel trace_marker.
-  int length = strlen(message);
-  char buf[length + WRITE_OFFSET];
-  size_t len = snprintf(buf, length + WRITE_OFFSET, "B|%d|%s", getpid(), message);
-
-  // Tracing may stop just after checking property and before writing the message.
-  // So the write is acceptable to fail. See b/20666100.
-  TEMP_FAILURE_RETRY(write(trace_marker_fd, buf, len));
+    (void)message;
 }
 
 void bionic_trace_end() {
-  if (!should_trace()) {
     return;
-  }
-
-  int trace_marker_fd = get_trace_marker_fd();
-  if (trace_marker_fd == -1) {
-    return;
-  }
-
-  TEMP_FAILURE_RETRY(write(trace_marker_fd, "E|", 2));
 }
 
 ScopedTrace::ScopedTrace(const char* message) : called_end_(false) {
